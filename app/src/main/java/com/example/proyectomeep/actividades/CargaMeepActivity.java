5ac370f98e98e46d78b7e3cbd08e5249171b6b64
Sebas6422 <91403654+Sebas6422@users.noreply.github.com>
2024-04-28 14:53:53 -0500

    subida del proyecto

---
+++ app/src/main/java/com/example/proyectomeep/actividades/CargaMeepActivity.java
@@ -0,0 +1,37 @@
+package com.example.proyectomeep.actividades;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.proyectomeep.R;
+
+public class CargaMeepActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_carga_meep);
+
+        Thread tcarga = new Thread(){
+            @Override
+            public void run(){
+                  super.run();
+                   try {
+                       sleep(3000);
+                   }catch (InterruptedException e){
+                       throw new RuntimeException(e);
+                   }
+                   finally {
+                       Intent iSesion = new Intent(getApplicationContext(), InicionSesionMeepActivity.class);
+
+                       startActivity(iSesion);
+
+                       finish();
+                   }
+            }
+        };
+        tcarga.start();
+    }
+}
---
+++ app/src/main/java/com/example/proyectomeep/actividades/InicionSesionMeepActivity.java
@@ -0,0 +1,16 @@
+package com.example.proyectomeep.actividades;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.proyectomeep.R;
+
+public class InicionSesionMeepActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicion_sesion_meep);
+    }
+}
