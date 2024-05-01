d391413637513bb5c3aec9e7dea8d5257e16b8aa
Sebas6422 <91403654+Sebas6422@users.noreply.github.com>
2024-05-01 02:59:19 -0500

    cambios_sebitas

---
+++ app/src/main/java/com/example/proyectomeep/actividades/RegistroActivity.java
@@ -0,0 +1,16 @@
+package com.example.proyectomeep.actividades;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.proyectomeep.R;
+
+public class RegistroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+    }
+}
