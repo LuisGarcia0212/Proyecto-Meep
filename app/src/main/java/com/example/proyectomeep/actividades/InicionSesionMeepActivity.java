96aaafd8edeff5a48bf4b1f83a9ab36a9870c8c0
chenaoh <TobeCA12381>
2024-05-05 02:05:12 -0500

    rama beto

--- app/src/main/java/com/example/proyectomeep/actividades/InicionSesionMeepActivity.java
+++ app/src/main/java/com/example/proyectomeep/actividades/InicionSesionMeepActivity.java
@@ -2,24 +2,34 @@ package com.example.proyectomeep.actividades;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.proyectomeep.R;
 
-public class InicionSesionMeepActivity extends AppCompatActivity implements View.OnClickListener{
+import java.util.Locale;
 
-    TextView lblRegistro;
+public class InicionSesionMeepActivity extends AppCompatActivity implements View.OnClickListener{
+    Spinner spinner;
+    TextView lblRegistro,lblRecuperacion,lblRestablecer ;
 
     EditText txtUser, txtPsw;
 
     Button btnIniciarSesion;
 
+
+    public static final String[] languages = {"Selecciona el lenguaje","Español","Ingles"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,19 +39,62 @@ public class InicionSesionMeepActivity extends AppCompatActivity implements View
         txtUser = findViewById(R.id.logTxtUser);
         txtPsw = findViewById(R.id.logTxtClave);
         btnIniciarSesion = findViewById(R.id.logBtnIniciar);
+        lblRestablecer = findViewById(R.id.logLblRestablecer1);
 
         btnIniciarSesion.setOnClickListener(this);
         lblRegistro.setOnClickListener(this);
+        lblRestablecer.setOnClickListener(this);
+
+
+
+        spinner = findViewById(R.id.spinner);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,languages);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setSelection(0);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedLang = parent.getItemAtPosition(position).toString();
+                if (selectedLang.equals("Español")){
+                    setLocal(InicionSesionMeepActivity.this,"es");
+                    finish();
+                    startActivity(getIntent());
+                } else if (selectedLang.equals("Ingles")) {
+                    setLocal(InicionSesionMeepActivity.this,"en");
+                    finish();
+                    startActivity(getIntent());
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+    public void setLocal(Activity activity,String langCode){
+        Locale locale= new Locale(langCode);
+        locale.setDefault(locale);
+        Resources resources = activity.getResources();
+        Configuration config =resources.getConfiguration();
+        config.setLocale(locale);
+        resources.updateConfiguration(config,resources.getDisplayMetrics());
     }
 
     @Override
     public void onClick(View v) {
         if(v.getId() == R.id.logBtnIniciar){
-            iniciarSesion();
+            iniciarSesion(txtUser.getText().toString().trim(), txtPsw.getText().toString());
         } else if (v.getId() == R.id.logLblRegistro1) {
             ingresarRegistro();
-        }
+        }else if (v.getId() == R.id.logLblRestablecer1) {
+            ingresarRestablecer();
     }
+    }
+
+
 
     private void ingresarRegistro(){
         Intent iRegistro = new Intent(this, RegistroActivity.class);
@@ -49,9 +102,21 @@ public class InicionSesionMeepActivity extends AppCompatActivity implements View
         finish();
     }
 
-    private void iniciarSesion(){
-        Intent iBienvenida = new Intent(this, BienvenidaActivity.class);
-        startActivity(iBienvenida);
+    private void iniciarSesion(String user, String psw){
+        if(user.equals("psyduck@gmail.com") && psw.equals("123456")) {
+            Intent iBienvenida = new Intent(this, BienvenidaActivity.class);
+            startActivity(iBienvenida);
+            finish();
+        }else {
+            Toast.makeText(this, "Usuario o contraseña incorrecta",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    private void ingresarRestablecer() {
+        Intent iRestablecer = new Intent(this, RestablecerContrasenhaActivity.class);
+        startActivity(iRestablecer);
         finish();
     }
-}
+    }
--- app/src/main/java/com/example/proyectomeep/actividades/RestablecerContrasenhaActivity.java
+++ app/src/main/java/com/example/proyectomeep/actividades/RestablecerContrasenhaActivity.java
@@ -2,15 +2,32 @@ package com.example.proyectomeep.actividades;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.proyectomeep.R;
 
-public class RestablecerContrasenhaActivity extends AppCompatActivity {
-
+public class RestablecerContrasenhaActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView lblRecuperar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restablecer_contrasenha);
+        lblRecuperar = findViewById(R.id.logLblRecuperar1);
+        lblRecuperar.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        if(v.getId() == R.id.logLblRecuperar1){
+            iniciarRecuperar();
+        }
+    }
+
+    private void iniciarRecuperar() {
+        Intent iBienvenida = new Intent(this, CodigoRecuperaActivity.class);
+        startActivity(iBienvenida);
+        finish();
     }
 }
