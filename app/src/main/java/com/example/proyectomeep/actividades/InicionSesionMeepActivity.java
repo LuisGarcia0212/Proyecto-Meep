ea20b3edbf56e1aa726a00f0fd0936c8df473870
Sebas6422 <91403654+Sebas6422@users.noreply.github.com>
2024-06-22 20:41:24 -0500

    arreglo del inicio de sesion

--- app/src/main/java/com/example/proyectomeep/actividades/InicionSesionMeepActivity.java
+++ app/src/main/java/com/example/proyectomeep/actividades/InicionSesionMeepActivity.java
@@ -11,6 +11,7 @@ import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -39,6 +40,7 @@ import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.gson.JsonArray;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.Base64;
 import com.loopj.android.http.BaseJsonHttpResponseHandler;
@@ -46,12 +48,15 @@ import com.loopj.android.http.RequestParams;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import cz.msebera.android.httpclient.Header;
 
@@ -67,9 +72,8 @@ public class InicionSesionMeepActivity extends AppCompatActivity implements View
 
     Button btnIniciarSesion;
 
-
-     ImageView btnfb;
-     private CallbackManager callbackManager;
+    ImageView btnfb;
+    private CallbackManager callbackManager;
 
      //beto
 
@@ -98,7 +102,7 @@ public class InicionSesionMeepActivity extends AppCompatActivity implements View
                 new FacebookCallback<LoginResult>() {
                     @Override
                     public void onSuccess(LoginResult loginResult) {
-                        handleFacebookAccessToken(loginResult.getAccessToken());
+                        //handleFacebookAccessToken(loginResult.getAccessToken());
                     }
 
                     @Override
@@ -122,44 +126,44 @@ public class InicionSesionMeepActivity extends AppCompatActivity implements View
         validarRecordarSesion();
     }
 
-    private void handleFacebookAccessToken(AccessToken token) {
-        AsyncHttpClient client = new AsyncHttpClient();
-        RequestParams params = new RequestParams();
-        params.put("token", token.getToken());
+//    private void handleFacebookAccessToken(AccessToken token) {
+//        AsyncHttpClient client = new AsyncHttpClient();
+//        RequestParams params = new RequestParams();
+//        params.put("token", token.getToken());
 
-        client.post(urlIniciarSesion, params, new BaseJsonHttpResponseHandler() {
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, String rawJsonResponse, Object response) {
-                try {
-                    JSONArray jsonArray = new JSONArray(rawJsonResponse);
-                    if (jsonArray.length() > 0) {
+//        client.post(urlIniciarSesion, params, new BaseJsonHttpResponseHandler() {
+//            @Override
+//            public void onSuccess(int statusCode, Header[] headers, String rawJsonResponse, Object response) {
+//                try {
+//                    JsonArray jsonArray = new JSONArray(rawJsonResponse);
+//                    if (jsonArray.length() > 0) {
                         // Usuario encontrado en la base de datos
-                        int id = jsonArray.getJSONObject(0).getInt("idUsuario");
-                        String user = jsonArray.getJSONObject(0).getString("Username");
-                        String password = jsonArray.getJSONObject(0).getString("Contraseña");
+//                        int id = jsonArray.getJSONObject(0).getInt("idUsuario");
+//                        String user = jsonArray.getJSONObject(0).getString("Username");
+//                        String password = jsonArray.getJSONObject(0).getString("Contraseña");
 
                         // Iniciar sesión con los datos obtenidos
-                        iniciarSesion(user, password, true);
-                    } else {
+//                        iniciarSesion(user, password, true);
+//                    } else {
                         // Usuario no encontrado en la base de datos
-                        Toast.makeText(getApplicationContext(), "Usuario no registrado", Toast.LENGTH_SHORT).show();
-                    }
-                } catch (JSONException e) {
-                    Toast.makeText(getApplicationContext(), "Error al procesar la respuesta del servidor", Toast.LENGTH_SHORT).show();
-                }
-            }
+//                        Toast.makeText(getApplicationContext(), "Usuario no registrado", Toast.LENGTH_SHORT).show();
+//                    }
+//                } catch (JSONException e) {
+//                    Toast.makeText(getApplicationContext(), "Error al procesar la respuesta del servidor", Toast.LENGTH_SHORT).show();
+//                }
+//            }
 
-            @Override
-            public void onFailure(int statusCode, Header[] headers, Throwable throwable, String rawJsonData, Object errorResponse) {
-                Toast.makeText(getApplicationContext(), "Error: " + statusCode, Toast.LENGTH_SHORT).show();
-            }
+//            @Override
+//            public void onFailure(int statusCode, Header[] headers, Throwable throwable, String rawJsonData, Object errorResponse) {
+//                Toast.makeText(getApplicationContext(), "Error: " + statusCode, Toast.LENGTH_SHORT).show();
+//            }
 
-            @Override
-            protected Object parseResponse(String rawJsonData, boolean isFailure) throws Throwable {
-                return null;
-            }
-        });
-    }
+//            @Override
+//            protected Object parseResponse(String rawJsonData, boolean isFailure) throws Throwable {
+//                return null;
+//            }
+//        });
+//    }
 
     private void validarRecordarSesion() {
         MEEP mp = new MEEP(this);
@@ -184,10 +188,7 @@ public class InicionSesionMeepActivity extends AppCompatActivity implements View
             ingresarRegistro();
         }else if (v.getId() == R.id.logLblRestablecer1) {
             ingresarRestablecer();
-    }
-
-
-
+        }
     }
 
     private void ingresarRegistro(){
@@ -196,68 +197,92 @@ public class InicionSesionMeepActivity extends AppCompatActivity implements View
         finish();
     }
 
-    private void iniciarSesion(String user, String psw, boolean recordar){
+    private void iniciarSesion(String user, String psw, boolean recordar) {
         MEEP mp = new MEEP(this);
         Hash hash = new Hash();
         AsyncHttpClient ahcIniciarSesion = new AsyncHttpClient();
         RequestParams params = new RequestParams();
 
-        psw = recordar == true ? psw : hash.StringToHash(psw, "SHA256");
-        params.add("usuario", user);
-        params.add("clave", psw);
+        psw = recordar ? psw : hash.StringToHash(psw, "SHA256");
+
+        if (verificarEmail(user)) {
+            params.add("clave", psw);
+            params.add("email", user);
+        } else {
+            params.add("usuario", user);
+            params.add("clave", psw);
+        }
 
         ahcIniciarSesion.post(urlIniciarSesion, params, new BaseJsonHttpResponseHandler() {
-
             @Override
             public void onSuccess(int statusCode, Header[] headers, String rawJsonResponse, Object response) {
-                if(statusCode == 200){
+                if (statusCode == 200) {
                     try {
-                        JSONArray jsonArray = new JSONArray(rawJsonResponse);
-                        if(jsonArray.length() > 0){
-                            int id = jsonArray.getJSONObject(0).getInt("idUsuario");
-                            if (id == -1)
+                        JSONObject jsonObject = new JSONObject(rawJsonResponse);
+                        if (jsonObject.getString("status").equals("success")) {
+                            JSONObject data = jsonObject.getJSONObject("data");
+                            int id = data.getInt("idUsuario");
+                            if (id == -1) {
                                 Toast.makeText(getApplicationContext(), "Credenciales incorrectas", Toast.LENGTH_SHORT).show();
-                            else {
+                            } else {
                                 Usuario usuario = new Usuario();
                                 usuario.setIdUsuario(id);
-                                usuario.setNombresC(jsonArray.getJSONObject(0).getString("NombresCompletos"));
-                                usuario.setUsuario(jsonArray.getJSONObject(0).getString("Username"));
-                                usuario.setClave(jsonArray.getJSONObject(0).getString("Contraseña"));
-                                usuario.setEmail(jsonArray.getJSONObject(0).getString("Email"));
-                                usuario.setTelefono(jsonArray.getJSONObject(0).getString("Telefono"));
-                                usuario.setDireccion(jsonArray.getJSONObject(0).getString("Direccion"));
-                                usuario.setDescripcion(jsonArray.getJSONObject(0).getString("Descripcion"));
-                                Uri imagenU = saveImageToTempFile(jsonArray.getJSONObject(0).getString("Foto"));
+                                usuario.setNombresC(data.getString("NombresCompletos"));
+                                usuario.setUsuario(data.getString("Username"));
+                                usuario.setClave(data.getString("Contraseña"));
+                                usuario.setEmail(data.getString("Email"));
+                                usuario.setTelefono(data.getString("Telefono"));
+                                usuario.setDireccion(data.getString("Direccion"));
+                                usuario.setDescripcion(data.getString("Descripcion"));
+                                Uri imagenU = saveImageToTempFile(data.getString("Foto"));
                                 usuario.setFoto(imagenU.toString());
-                                if(chkRecordar.isChecked())
+                                if (chkRecordar.isChecked()) {
                                     mp.agregarUsuario(usuario.getIdUsuario(), usuario.getUsuario(), usuario.getClave());
+                                }
                                 Intent iBienvenida = new Intent(getApplicationContext(), BienvenidaActivity.class);
                                 iBienvenida.putExtra("usuario", usuario);
                                 startActivity(iBienvenida);
                                 finish();
                             }
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Credenciales incorrectas", Toast.LENGTH_SHORT).show();
                         }
                     } catch (JSONException e) {
-                        throw new RuntimeException(e);
+                        Toast.makeText(getApplicationContext(), "Error al procesar la respuesta del JSON.", Toast.LENGTH_SHORT).show();
                     } catch (IOException e) {
-                        throw new RuntimeException(e);
+                        Toast.makeText(getApplicationContext(), "Error al procesar la respuesta del servidor.", Toast.LENGTH_SHORT).show();
                     }
-
-                }else {
-                    Toast.makeText(getApplicationContext(), "NO entro", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Error en el servidor: " + statusCode, Toast.LENGTH_SHORT).show();
                 }
             }
 
             @Override
             public void onFailure(int statusCode, Header[] headers, Throwable throwable, String rawJsonData, Object errorResponse) {
-                Toast.makeText(getApplicationContext(), "Error: "+statusCode, Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Error: " + statusCode, Toast.LENGTH_SHORT).show();
             }
 
             @Override
             protected Object parseResponse(String rawJsonData, boolean isFailure) throws Throwable {
-                return null;
+                try {
+                    return new JSONObject(rawJsonData);
+                } catch (JSONException e) {
+                    Log.e("JSON Exception", "Error al convertir la respuesta a JSON: " + e.getMessage());
+                    return null;
+                }
             }
         });
+    }
+
+
+    private boolean verificarEmail(String email) {
+        if (email == null || email.isEmpty()) {
+            return false;
+        }
+
+        Pattern p = Pattern.compile("^[A-Za-z0-9_-]+@(gmail\\.com|hotmail\\.com)$");
+        Matcher m = p.matcher(email);
+        return m.find();
     }
 
     private Uri saveImageToTempFile(String base64Image) throws IOException {
